9c2178383e33127b73306ad4d4f3cc0f4c87e9f8
didi <didi@qq.com>
2019-10-12 09:03:38 +0800

    init v2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'lib-flexible';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-serviceWorker.unregister();
